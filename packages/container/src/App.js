f18968d9d604bab67b0dfe3dbbb82081d916a8e0
kcabhish <kcabhish@gmail.com>
2025-05-14 10:44:49 -0400

    fix: adding name space for class generator in css-in-js

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,14 +2,25 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { MarketingApp } from './components/marketingApp';
 import Header from './components/Header';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
+
+const generateClassName = createGenerateClassName({
+  // instead of generating classnames as jss1, jss2 it will create ma1, ma2
+  productionPrefix: 'co',
+});
 
 export default () => {
   return (
     <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
     </BrowserRouter>
   );
 };
