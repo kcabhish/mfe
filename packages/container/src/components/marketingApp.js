d0cb4df6377d0fff4b8bc3d097b5949180a5c3f1
kcabhish <kcabhish@gmail.com>
2025-05-14 13:35:27 -0400

    feat: adding memory history in sub-app and syncing with browser history

--- packages/container/src/components/marketingApp.js
+++ packages/container/src/components/marketingApp.js
@@ -1,11 +1,30 @@
 import React, {useRef, useEffect} from 'react';
 import { mount } from 'marketing/MarketingApp';
+import { useHistory } from 'react-router-dom';
 
+/**
+ * This component creates a ref for its element and passes it on mount to load the markting app.
+ * @returns 
+ */
 const MarketingApp = () => {
     const ref = useRef(null);
+    /**
+     * This is the copy of the browser history. Since we are using memory router in the sub-apps,
+     * we need to sync the information back the the browser history.
+     */
+    const history = useHistory();
 
     useEffect(() => {
-        mount(ref.current);
+        mount(ref.current, {
+            onNavigate: ({pathname: nextPathname}) => {
+                // prevent navigation if current pathname and next pathname are same
+                const pathname = history.location;
+                if (pathname !== nextPathname) {
+                    history.push(nextPathname);
+                }
+                
+            }
+        });
     });
     return <div ref={ref} />;
 };
--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,26 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createMemoryHistory } from 'history';
 
 /**
  * Mount function to start up the project
- * @param {*} el 
+ * @param {*} el
  */
-const mount = (el) => {
-    ReactDOM.render(
-        <App />, el
-    )
-}
+const mount = (el, { onNavigate }) => {
+  const history = createMemoryHistory();
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+
+  ReactDOM.render(<App history={history} />, el);
+};
 
 /**
  * Check to see if the code is running in development or production
  */
 
-if (process.env.NODE_ENV==='development') {
-    const devRoot = document.querySelector('#_marketing-dev-root');
-    if (devRoot) {
-        mount(devRoot);
-    }
+if (process.env.NODE_ENV === 'development') {
+  const devRoot = document.querySelector('#_marketing-dev-root');
+  if (devRoot) {
+    mount(devRoot, {});
+  }
 }
 
 export { mount };
