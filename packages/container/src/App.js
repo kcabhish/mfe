6cab754a7091f12d537665d5176a51869c4e54b7
Abhishek Kc <kcabhish@gmail.com>
2025-05-15 01:18:23 -0400

    feat: Adding dashboard build in vue  (#2)
    * feat: setting up dashboard in vue
    
    * feat: setting up dashboard to the container
    
    * feat: adding logic to display dashboard on signin
    
    * chore: added GHA for dashboard

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,5 +1,6 @@
-import React, { lazy, Suspense, useState } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React, { lazy, Suspense, useState, useEffect } from 'react';
+import { Router, Redirect, Route, Switch } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
 import {
   StylesProvider,
   createGenerateClassName,
@@ -9,16 +10,26 @@ import Header from './components/Header';
 
 const MarketingLazy = lazy(() => import('./components/marketingApp'));
 const AuthLazy = lazy(() => import('./components/authApp'));
+const DashboardLazy = lazy(() => import('./components/DashboardApp'));
 
 const generateClassName = createGenerateClassName({
   // instead of generating classnames as jss1, jss2 it will create ma1, ma2
   productionPrefix: 'co',
 });
 
+const history = createBrowserHistory();
+
 export default () => {
   const [isSignedIn, setIsSigndIn] = useState(false);
+
+  useEffect(() => {
+    if (isSignedIn) {
+      history.push('/dashboard');
+    }
+  },[isSignedIn]);
+
   return (
-    <BrowserRouter>
+    <Router history={history}>
       <StylesProvider generateClassName={generateClassName}>
         <div>
           <Header isSignedIn={isSignedIn} onSignOut={() => setIsSigndIn(false)}/>
@@ -27,11 +38,15 @@ export default () => {
               <Route path="/auth">
                 <AuthLazy onSignIn={() => setIsSigndIn(true)}/>
               </Route>
+              <Route path="/dashboard">
+               {!isSignedIn && <Redirect to="/" />}
+                <DashboardLazy />
+              </Route>
               <Route path="/" component={MarketingLazy}></Route>
             </Switch>
           </Suspense>
         </div>
       </StylesProvider>
-    </BrowserRouter>
+    </Router>
   );
 };
