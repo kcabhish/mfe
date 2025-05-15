9475aa6c3476333bb6bdecd43acf52dd6375daf2
kcabhish <kcabhish@gmail.com>
2025-05-14 23:58:12 -0400

    feat: adding lazy loading for performance

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { MarketingApp } from './components/marketingApp';
-import { AuthApp } from './components/authApp';
-import Header from './components/Header';
 import {
   StylesProvider,
   createGenerateClassName,
 } from '@material-ui/core/styles';
+import Progress from './components/Progress';
+import Header from './components/Header';
 
+const MarketingLazy = lazy(() => import('./components/marketingApp'));
+const authLazy = lazy(() => import('./components/AuthApp'));
 
 const generateClassName = createGenerateClassName({
   // instead of generating classnames as jss1, jss2 it will create ma1, ma2
@@ -20,10 +21,12 @@ export default () => {
       <StylesProvider generateClassName={generateClassName}>
         <div>
           <Header />
-          <Switch>
-            <Route path='/auth' component={AuthApp}></Route>
-            <Route path="/" component={MarketingApp}></Route>
-          </Switch>
+          <Suspense fallback={<Progress />}>
+            <Switch>
+              <Route path="/auth" component={authLazy}></Route>
+              <Route path="/" component={MarketingLazy}></Route>
+            </Switch>
+          </Suspense>
         </div>
       </StylesProvider>
     </BrowserRouter>
